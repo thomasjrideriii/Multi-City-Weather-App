3bfb6da94a5900be3176cfac69ce9fd40457ebb9
Thomas Rider <thomasjrideriii@gmail.com>
2020-05-13 19:43:39 -0400

    Finished 5 day forecast functionality and added visual changes

--- Assets/script.js
+++ Assets/script.js
@@ -12,7 +12,7 @@ function renderSearches() {
 
   for (i = 0; i < pastSearches.length; i++) {
     $("#pastSearches").append(
-      $("<li class='list-group-item city'>").text(pastSearches[i])
+      $("<li class='list-group-item city bg-dark text-light'>").text(pastSearches[i])
     );
   }
 }
@@ -22,6 +22,8 @@ function displayWeather(city) {
     // var apiKey = "4e48182efcaca931751b0b92eccfb27d"
     var queryURLmain = "https://api.openweathermap.org/data/2.5/weather?appid=4e48182efcaca931751b0b92eccfb27d&units=imperial&q=" + city
 
+
+    // Populates main Weather Chart
     $.ajax({
         url: queryURLmain,
         method: "GET"
@@ -35,8 +37,11 @@ function displayWeather(city) {
         cityLon = response.coord.lon
         cityLat = response.coord.lat
         // console.log(cityLat + ", " + cityLon)
-        queryURLuv = "http://api.openweathermap.org/data/2.5/uvi?appid=4e48182efcaca931751b0b92eccfb27d&lat=" + cityLat + "&lon=" + cityLon
 
+        // Takes lat and lon from previous object and finds the UV Index.
+        
+        queryURLuv = "http://api.openweathermap.org/data/2.5/uvi?appid=4e48182efcaca931751b0b92eccfb27d&lat=" + cityLat + "&lon=" + cityLon
+        
         $.ajax({
             url: queryURLuv,
             method: "GET"
@@ -44,27 +49,40 @@ function displayWeather(city) {
             // console.log(response)
             indexNumber = response.value.toFixed(2)
             $("#uvIndex").text(indexNumber)
-            if (indexNumber <= 3) {
-                $("#uvIndex").addClass("bg-success p-2 rounded-pill text-light")
-            } else if (indexNumber <= 6) {
-                $("#uvIndex").addClass("bg-warning p-2 rounded-pill")
+            $("#uvIndex").attr("class", "")
+            if (indexNumber < 3) {
+                $("#uvIndex").addClass("bg-success p-2 rounded-pill text-light font-weight-bold")
+            } else if (indexNumber < 6) {
+                $("#uvIndex").addClass("bg-warning p-2 rounded-pill font-weight-bold")
             } else {
-                $("#uvIndex").addClass("bg-danger p-2 rounded-pill text-light")
+                $("#uvIndex").addClass("bg-danger p-2 rounded-pill text-light font-weight-bold")
             }
         })
 
     })
 
-    var queryURLforecast = "https://api.openweathermap.org/data/2.5/forecast?appid=4e48182efcaca931751b0b92eccfb27d&q=" + city
+    // Calls for 5 day forecast information and populates chart.
+    var queryURLforecast = "https://api.openweathermap.org/data/2.5/forecast?appid=4e48182efcaca931751b0b92eccfb27d&units=imperial&q=" + city
 
     $.ajax ({
         url: queryURLforecast,
         method: "GET"
     }).then(function(response) {
         console.log(response)
+        // Runs 5 times, one for each box
         for (i=0; i<5; i++) {
+            $("#forecast"+i).empty()
             var foreDate = moment().add(i+1, 'days').format("L")
             $("#forecast"+i).append($("<h5>").text(foreDate))
+            
+            // gives us a search value for the incoming API array.
+            var newIndex = i * 8 + 6;
+            // console.log(response.list[newIndex])
+            $("#forecast"+i).append($("<img>").attr("src", "http://openweathermap.org/img/wn/" + response.list[newIndex].weather[0].icon + "@2x.png"))
+            $("#forecast"+i).append($("<p>").text("Temp: " + response.list[newIndex].main.temp + "°F"))
+            $("#forecast"+i).append($("<p>").text("Humidity: " + response.list[newIndex].main.humidity + "%"))
+
+            console.log(response.list[newIndex].main.temp)
         }
 
     })
@@ -101,3 +119,7 @@ $(document).on("click", ".city", function () {
 });
 
 renderSearches();
+
+if (pastSearches[0] !== null) {
+    displayWeather(pastSearches[0])
+}
