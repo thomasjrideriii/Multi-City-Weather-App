68e1a8b1a74d88844e9f07c0b4010fb08fcef3d6
Thomas Rider <thomasjrideriii@gmail.com>
2020-05-13 17:56:29 -0400

    Added fields for weather inputs and forecast

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,26 @@
 var pastSearches = JSON.parse(localStorage.getItem("searchHistory")) || [];
 
+var targetCity = "";
+
+// Render stored city data, called at refresh and when a new city is added.
 function renderSearches() {
-    $("#pastSearches").empty()
+  $("#pastSearches").empty();
 
-    for (i = 0; i < pastSearches.length; i++) {
-
-        $("#pastSearches").append($("<li class='list-group-item city'>").text(pastSearches[i]))
-    }
+  for (i = 0; i < pastSearches.length; i++) {
+    $("#pastSearches").append(
+      $("<li class='list-group-item city'>").text(pastSearches[i])
+    );
+  }
 }
 
-$("#cityForm").on("submit", function (event) {
-    event.preventDefault();
+// Displays weather data for the selected city, called when a new city is added and when a stored city is selected
+function displayWeather(city) {}
 
+// Adds a new city
+$("#cityForm").on("submit", function (event) {
+  event.preventDefault();
+
+  if ($("#newCity").val() !== "") {
     var city = $("#newCity").val().trim();
     // console.log(city)
     pastSearches.push(city);
@@ -19,20 +28,21 @@ $("#cityForm").on("submit", function (event) {
     localStorage.setItem("searchHistory", JSON.stringify(pastSearches));
 
     $("#newCity").val("");
-    
-    console.log(pastSearches)
 
-    console.log(JSON.parse(localStorage.getItem("searchHistory")))
+    console.log(pastSearches);
 
-    renderSearches()
+    console.log(JSON.parse(localStorage.getItem("searchHistory")));
+
+    renderSearches();
+
+    displayWeather(city);
+  }
 });
 
-$(document).on("click", ".city", function(){
-    var targetCity = $(this).text();
-    console.log(targetCity)
-    
-})
+$(document).on("click", ".city", function () {
+  targetCity = $(this).text();
+  console.log(targetCity);
+  displayWeather(targetCity);
+});
 
-
-
-renderSearches()
+renderSearches();
